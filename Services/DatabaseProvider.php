b858d52f1a3eac723e43951d45cf75c11e8a139c
Omar El-Mihilmy <mihilmy@yahoo.com>
2017-05-02 09:59:10 -0400

    updated database provider

--- Services/DatabaseProvider.php
+++ Services/DatabaseProvider.php
@@ -26,123 +26,12 @@
 			mysqli_close($this->conn);
 		}
 
-		private function publishStudents(){
-			$query = <<<DEMO
-				CREATE TABLE IF NOT EXISTS students ( 
-				    directory_ID INT(9) AUTO_INCREMENT PRIMARY KEY,
-    				name varchar(255) NOT NULL,
-    				email varchar(255) NOT NULL,
-					password varchar(255) NOT NULL,
-					reg_date TIMESTAMP
-					)
-DEMO;
-
-
-			 if (mysqli_query($this->conn, $query) === false){
-			 	echo 'ERROR PUBLISING STUDENTS';
-			 }
-			 	
-		}
-
-		private function publishTeachers(){
-			$query = <<<DEMO
-				CREATE TABLE IF NOT EXISTS teachers ( 
-				    directory_ID INT(9) AUTO_INCREMENT PRIMARY KEY,
-    				name varchar(255) NOT NULL,
-    				email varchar(255) NOT NULL,
-					password varchar(255) NOT NULL,
-					reg_date TIMESTAMP
-					)
-DEMO;
-
-
-			 if (mysqli_query($this->conn, $query) === false){
-			 	echo 'ERROR PUBLISING TEACHERS';
-			 }
-		}
-
-		private function publishClasses(){
-			$query = <<<DEMO
-				CREATE TABLE IF NOT EXISTS classes ( 
-    				name varchar(255) NOT NULL PRIMARY KEY,
-    				teacherID int,
-    				FOREIGN KEY(teacherID)	REFERENCES teachers(directory_ID)
-    				)
-DEMO;
-
-
-			 if (mysqli_query($this->conn, $query) === false){
-			 	echo 'ERROR PUBLISING CLASSES';
-			 }
-		}
-
-		private function publishAssignments(){
-			$query = <<<DEMO
-				CREATE TABLE IF NOT EXISTS assignments ( 
-    				assignment_ID INT(9) AUTO_INCREMENT PRIMARY KEY,
-    				name varchar(255) NOT NULL,
-    				class_name varchar(255),
-    				due_date DATETIME NOT NULL,
-    				max_score int NOT NULL,
-    				FOREIGN KEY(class_name) REFERENCES classes(name)
-    				)
-DEMO;
-
-
-			 if (mysqli_query($this->conn, $query) === false){
-			 	echo 'ERROR PUBLISING assignments';
-			 }
-		}
-
-		private function publishStudent_Classes(){
-			$query = <<<DEMO
-				CREATE TABLE IF NOT EXISTS student_classes ( 
-    				student_ID INT NOT NULL,
-    				class_name varchar(255) NOT NULL,
-    				FOREIGN KEY(student_ID) REFERENCES students(directory_ID),
-    				FOREIGN KEY(class_name) REFERENCES classes(name)
-    				)
-DEMO;
-
-
-			 if (mysqli_query($this->conn, $query) === false){
-			 	echo 'ERROR PUBLISING student_classes';
-			 }
-		}
-
-		private function publishTeacher_Classes(){
-			$query = <<<DEMO
-				CREATE TABLE IF NOT EXISTS teacher_classes ( 
-    				teacher_id INT NOT NULL,
-    				class_name varchar(255) NOT NULL,
-    				FOREIGN KEY(teacher_id) REFERENCES teachers(directory_ID),
-    				FOREIGN KEY(class_name) REFERENCES classes(name)
-    				)
-DEMO;
-
-
-			 if (mysqli_query($this->conn, $query) === false){
-			 	echo 'ERROR PUBLISING teachers_classes';
-			 }
-		}
-
-		public function publish(){
-			$this->publishStudents();
-			$this->publishTeachers();
-			$this->publishClasses();
-			$this->publishAssignments();
-			$this->publishStudent_Classes();
-			$this->publishTeacher_Classes();
-		}
-
-		public static function getInstance()
-		{
+		public static function getInstance(){
 			// Check is $_instance has been set
 			if(!isset(self::$instance)) 
 			{
 				// Creates sets object to instance
 				self::$instance = new DatabaseProvider();
-				self::$instance->publish();
 			}
 
 			// Returns the instance
