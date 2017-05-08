7af74be1f00e72aef7b01a855beea972bf968456
Abdelrahman Hammad <ahammad@terpmail.umd.edu>
2017-05-07 22:21:16 -0400

    Showing Submissions
    Showing submissions for each student under the assignment

--- Assignments/show.php
+++ Assignments/show.php
@@ -2,6 +2,8 @@
 require_once("../Services/DatabaseProvider.php");
 require_once("../students/student.php");
 require_once("../Teachers/teacher.php");
+require_once("../Submissions/submissionsController.php");
+
 	session_start();
 	if(!isset($_GET['assignmentid']) || (!isset($_SESSION['current_student']) && !isset($_SESSION['current_teacher']))) {
 		header("Location: ./showClasses.php");
@@ -113,6 +115,10 @@ H;
 }
 
 			 if(!isset($_SESSION['current_teacher']))  {
+			 	//Call the show() function from the submission controller
+			 	show();
+			 		
+
 							echo <<<H
 							<div class="col-sm">
 							<br>
